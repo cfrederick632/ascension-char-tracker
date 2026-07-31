6386125c23b008bed042aaa80983b4da1266bf1c
cfrederick632 <1deaged@gmail.com>
2026-07-31 15:05:59 -0400

    kkkk

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
+    console.log("Current Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
     fetchCharacters();
   }, []);
 
